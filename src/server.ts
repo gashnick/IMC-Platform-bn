d76443879110a26bb4ab312055232cd0f6125206
Saveur Bikorimana <bikorimanaxavier@gmail.com>
2024-12-26 11:17:50 +0200

    fixed cors problem

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({ 
-    origin: ["https://imc-platform.netlify.app", "https://imc-platform-bn.onrender.com", "http://localhost:3000", "https://deploy-preview-11--imc-platform.netlify.app"], 
+    origin: env.CORS_ORIGIN, 
     credentials: true 
 }));
 
